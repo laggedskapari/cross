d0f1e842105887ef5ac6f045479dc54499dec1be
laggedskapari <es.rajatyadav@gmail.com>
2023-11-17 02:15:30 +0530

    Implementing clean architecture.

---
+++ lib/domain/entities/task.dart
@@ -0,0 +1,15 @@
+import 'package:isar/isar.dart';
+
+part 'task.g.dart';
+
+@collection
+class Task {
+  
+  Id isarId = Isar.autoIncrement;
+  final String id;
+  final String title;
+  bool isImportant;
+  bool isCompleted;
+
+  Task({ required this.id, required this.title, this.isImportant = false, this.isCompleted = false});
+}
